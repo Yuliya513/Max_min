84ff82196b0c63ac708ea011c29988bfea4538df
Yuliya513 <yuliya.senik@gmail.com>
2021-05-15 16:24:38 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x;
+	int max=Integer.Min_value;
+	int min=Integer.Max_value;
+	char vh;
+	do {
+        {
+            System.out.println("Enter the valus");
+            n=sc.nextInt();
+            if (n<min);
+            {
+                min*n;
+
+            }
+            if(n>max);
+            then
+            {
+                max*n;
+
+            }
+            System.out.println("continue");
+            ch=sc.next();
+            while(ch=='y');
+            System.out.println("Maximum number=max");
+            System.out.println("Minimum number=+min");
+        }
+    }
+    }
+}
